6a3df33766354d914369ed5dd8d0858e0e0f0c22
Nishant Usapkar <cool250@outlook.com>
2015-11-17 12:52:54 -0800

    Made port number changes

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use('/node_modules', express.static(__dirname + '/node_modules'));
 // Add static middleware for other static application file
 app.use('/web', express.static(__dirname + '/web'));
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 5000;
 
 app.listen(port);
 
-console.log("Running at Port " + port);
+console.log("Running on Port " + port);

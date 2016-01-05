e8e635b65099beea963143b43cc1856b5668325b
nusapka <cool250@outlook.com>
2016-01-05 12:38:00 -0800

    Removed fields causing errors

--- web/src/components/CreateInterviewForm.jsx
+++ web/src/components/CreateInterviewForm.jsx
@@ -6,17 +6,9 @@ class CreateInterviewForm extends React.Component {
         return (
             <form className="form-inline" role="form">
                 <div className="form-group">
-                    <label className="sr-only" for="email">Email address:</label>
-                    <input type="email" class="form-control" id="email"></input>
+                    <label className="sr-only" htmlFor="email">Email address:</label>
+                    <input type="email" className="form-control" id="email"></input>
                 </div>
-                <div class="form-group">
-                    <label className="sr-only" for="pwd">Password:</label>
-                    <input type="password" class="form-control" id="pwd"></input>
-                </div>
-                <div className="checkbox">
-                    <label><input type="checkbox"> Remember me</input></label>
-                </div>
-                <button type="submit" className="btn btn-default">Submit</button>
             </form>
         )
     }

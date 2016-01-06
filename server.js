fe51c469131de8a1337eecca44468a53ac497a9b
askashish <sharashish@gmail.com>
2016-01-05 22:51:00 -0800

    added open browser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app     = express();
 var path    = require("path");
+var open    = require("open");
 
 // API Routes
 var api = require('./server/routes/api');
@@ -14,6 +15,8 @@ app.use('/', express.static(__dirname + '/web'));
 
 var port = process.env.PORT || 5000;
 
-app.listen(port);
-
-console.log("Running on Port " + port);
+app.listen(port, 'localhost', function() {
+    console.log("Running on Port " + port);
+    console.log("Launching browser");
+    open('http://localhost:5000/');
+});
